7f2fb8469767da067bc005aa62ae2001b8606182
Douwe <douwewesterdijk@gmail.com>
2024-12-17 15:30:26 +0100

    Update GunHolder.cs

--- Assets/Scripts/GunHolder.cs
+++ Assets/Scripts/GunHolder.cs
@@ -3,16 +3,12 @@ using UnityEngine;
 public class GunHolder : MonoBehaviour
 {
     public Transform cameraTransform;
-    public Transform gunTransform;
-
-    void Start()
-    {
-
-    }
+    public Vector3 offset = new Vector3(0.3f, 0.5f, 0.3f);// Set this to position the gun on the right side
 
     void Update()
     {
-        transform.position = gunTransform.position;
+        // Position the GunHolder relative to the camera
+        transform.position = cameraTransform.position + cameraTransform.TransformDirection(offset);
         transform.rotation = cameraTransform.rotation;
     }
 }
