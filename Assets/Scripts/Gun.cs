faa8809b49c8605158cb001b01ccfe7d8598ff31
Rowaaantje <114189377+Rowaaantje@users.noreply.github.com>
2024-12-19 12:47:06 +0100

    [+] Fixed Items in GunHolder don't rotate w player
    Items in GunHolder don't rotate with the player when rotating the camera. FIXED

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,14 +2,38 @@ using UnityEngine;
 
 public class Gun : HoldableObject
 {
-    void Start()
-    {
 
-    }
+    public int damage;
+    public int fireRate;
+    private float nextFire;
+    public Camera camera;
 
-    // Update is called once per frame
+    [Header("VFX")]
+    public GameObject hitVFX;
+
     void Update()
     {
+        if (nextFire > 0)
+        {
+            nextFire -= Time.deltaTime;
+        }
 
+        if (Input.GetMouseButton(0) && nextFire <= 0)
+        {
+            nextFire = 1 / fireRate;
+            Fire();
+        }
+    }
+    
+    void Fire()
+    {
+        Ray ray = new Ray(camera.transform.position, camera.transform.forward);
+        
+        RaycastHit  hit;
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, 100f))
+        {
+            Instantiate(hitVFX, hit.point, Quaternion.identity);
+        }
     }
 }
--- Assets/Scripts/GunHolder.cs
+++ Assets/Scripts/GunHolder.cs
@@ -2,13 +2,5 @@ using UnityEngine;
 
 public class GunHolder : MonoBehaviour
 {
-    public Transform cameraTransform;
-    public Vector3 offset = new Vector3(0.3f, 0.5f, 0.3f);// Set this to position the gun on the right side
-
-    void Update()
-    {
-        // Position the GunHolder relative to the camera
-        transform.position = cameraTransform.position + cameraTransform.TransformDirection(offset);
-        transform.rotation = cameraTransform.rotation;
-    }
+    
 }
